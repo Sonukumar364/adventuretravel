919e85270507f9abb0d8397f9061b464e64c6146
Sonu Kumar <99730258+Sonukumar364@users.noreply.github.com>
2024-04-25 22:02:10 +0530

    Add files via upload

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer_container">
+      @Tripkaro 2024 | Maahi Shivhare
+    </div>
+  )
+}
+
+export default Footer
